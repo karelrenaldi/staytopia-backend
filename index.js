8334f0fb4e08355396642e67a63b01f785487ec7
karel renaldi <karelrenaldi8@gmail.com>
2021-07-04 10:49:24 +0700

    setup mongodb atlas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 import { DEFAULT_PORT, API_VERSION } from './configs/server';
 import routes from './routes';
@@ -27,8 +28,21 @@ const main = () => {
     })
   })
 
-  app.listen(port, () => {
-    console.log(`Running on : ${process.env.BASE_URL}`);
+  mongoose.connect(process.env.ATLAS_SERVER, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,  
+  }).then(() => {
+    app.listen(port, () => {
+      console.log('Successful connect to database');
+      console.log(`Running on : ${process.env.BASE_URL}`);
+    }).on('error', err => {
+      if (err) {
+        process.exit(1);
+      }
+    });
+  }).catch(() => {
+    console.log('Failed connect to database');
   })
 }
 
