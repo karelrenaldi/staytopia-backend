051bacd6e786646b190d41ca6bb279e6cce35ca6
karel renaldi <karelrenaldi8@gmail.com>
2021-07-04 12:48:25 +0700

    add spesific retrieve hotel

--- handlers/hotel.js
+++ handlers/hotel.js
@@ -1,5 +1,9 @@
+import mongodb from 'mongodb';
+
 import Hotel, { isHotel } from '../models/Hotel';
 import { API_VERSION } from '../configs/server';
+
+const { ObjectID } = mongodb;
 
 export const createHotel = async(req, res) => {
   const data = req.body;
@@ -28,6 +32,46 @@ export const createHotel = async(req, res) => {
       error: {
         code: 500,
         message: 'Internal server error when create new hotel!',
+      }
+    });
+  }
+}
+
+export const retrieveSpesificHotel = async(req, res) => {
+  const { hotelId } = req.params;
+
+  if(!ObjectID.isValid(hotelId)) return res.status(400).json({
+    apiVersion : API_VERSION,
+    error : {
+      code: 400,
+      message: 'Invalid hotelId'
+    }
+  })
+
+  try{
+    const hotel = await Hotel.findById(hotelId);
+
+    if(!hotel) return res.status(404).json({
+      apiVersion: API_VERSION,
+      error: {
+        code: 404,
+        message: 'Hotel with specified id not found!'
+      }
+    });
+
+    return res.json({
+      apiVersion: API_VERSION,
+      data: hotel,
+    });
+  }catch{
+    console.log("Error when trying to retrieve spesific hotel!");
+    console.log(err);
+
+    return res.status(500).json({
+      apiVersion: API_VERSION,
+      error: {
+        code: 500,
+        message: 'Internal server error when retrieve spesific hotel!',
       }
     });
   }
--- routes/global.js
+++ routes/global.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import { createHotel, retrieveHotel } from '../handlers/hotel';
+import { createHotel, retrieveSpesificHotel } from '../handlers/hotel';
 
 const globalRouter = Router();
 
+globalRouter.get('/hotels/:hotelId', retrieveSpesificHotel);
 globalRouter.route('/hotels').post(createHotel);
 
 export default globalRouter;
